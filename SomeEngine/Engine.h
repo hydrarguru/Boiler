a14e95c14060e36e93113609f65746d6a1f0fa63
Henrik Engqvist <henrik.engqvist@futuregames.nu>
2020-08-25 18:59:24 +0200

    Initial commit 2

---
+++ SomeEngine/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+//SFML
+#include "SFML/System.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
